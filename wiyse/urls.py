11154e4569f6083e4068860babd5676322ebc514
Vinay Mishra <vinaysanny78@gmail.com>
2022-12-01 19:46:50 +0530

    tech commit

--- wiyse/urls.py
+++ wiyse/urls.py
@@ -12,8 +12,7 @@ urlpatterns = [
 ]
 urlpatterns = [
     path('api/v1/', include('fundamentals.urls')),
-    path('api/v2/', include('fundamentals.urls')),
-    path('api/v3/', include('fundamentals.urls')),
+    
 ]
 urlpatterns = [
     path('api/v1/', include('fundamentals.urls')),
